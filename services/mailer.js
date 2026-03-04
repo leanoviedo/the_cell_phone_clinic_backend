60965fcbcdad95d4695ce0e765b987dce0040d6f
leanoviedo <leanoviedo@gmail.com>
2026-03-03 22:13:35 -0300

    refactory emails

--- services/mailer.js
+++ services/mailer.js
@@ -7,9 +7,7 @@ function getTransporter() {
   if (transporter) return transporter;
 
   transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
