6671c817d40452a68ae2339048a29df680e58f70
leanoviedo <leanoviedo@gmail.com>
2026-08-27 22:19:43 -0300

    fixed error server

--- api/index.js
+++ api/index.js
@@ -1,15 +1,7 @@
-require("dotenv").config();
-const app = require("../app");
-const connectDB = require("../config/db");
-
-let isConnected = false;
-
-module.exports = async (req, res) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-    console.log("✅ MongoDB conectado (Vercel)");
-  }
-
-  return app(req, res);
+module.exports = (req, res) => {
+  res.status(200).json({
+    message: "🚀 Vercel funciona",
+    url: req.url,
+    method: req.method,
+  });
 };
